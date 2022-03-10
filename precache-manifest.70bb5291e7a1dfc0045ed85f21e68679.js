edd4be776c9280d630746b016795aa4fe6da0d84
Cody Grosse <codygrosse@Codys-MacBook-Pro.local>
2022-03-10 15:22:14 -0500

    Updates

---
+++ precache-manifest.70bb5291e7a1dfc0045ed85f21e68679.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a88c214a07d72bb2d73",
+    "url": "/therobofriends/static/css/main.ad2d0677.chunk.css"
+  },
+  {
+    "revision": "4a88c214a07d72bb2d73",
+    "url": "/therobofriends/static/js/main.4a88c214.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/therobofriends/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "c85a73f5c6e8e5500ba9",
+    "url": "/therobofriends/static/css/2.40b18af0.chunk.css"
+  },
+  {
+    "revision": "c85a73f5c6e8e5500ba9",
+    "url": "/therobofriends/static/js/2.c85a73f5.chunk.js"
+  },
+  {
+    "revision": "0fc8ce87a79ba1d6cf34",
+    "url": "/therobofriends/static/js/runtime~main.0fc8ce87.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/therobofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "b85e9ae22b007e176c9af9608ebd5f9b",
+    "url": "/therobofriends/index.html"
+  }
+];
